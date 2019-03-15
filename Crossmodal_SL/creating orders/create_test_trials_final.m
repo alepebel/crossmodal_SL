0c7341567649fb00e913aa54b1b0084963b3da66
Alex <alexisperezbellido@gmail.com>
2019-03-15 18:23:09 +0100

    Add files via upload
    changing creating orders functions for a reduce test design

--- Crossmodal_SL/creating orders/create_test_trials_final.m
+++ Crossmodal_SL/creating orders/create_test_trials_final.m
@@ -1,26 +1,24 @@
 function create_test_trials_final()
 standard = reshape(1:6,[3,2]);
 deviants_mapping_x_modality = [
-    4 1; 5 2; 6 3; %inverse
     1 5; 2 6; 3 4; %dev1
-    1 6; 2 4; 3 5  %dev2
     ];
 
 %deviant_types_x_modality = repelem(1:3,3)';
-trials_x_modality = [repelem(1:6,3)', [repmat(7:9,[1 3])' ; repmat(10:12,[1 3])']];
+trials_x_modality = [repelem(1:3,3)', repmat(4:6,[1 3])'];
 
-trial_conditions = [repelem(1:4,18);repmat(repelem(1:2,9),[1 4])]'; %first col:modality; second col:trial type
-trial_answers = [ones(72*2,1),zeros(72*2,1)];
+trial_conditions = [repelem(1:4,9)]' %first col:modality;
+trial_answers = [ones(36,1),zeros(36,1)];
 
 pair_keys = [standard; deviants_mapping_x_modality];
 trial_keys = trials_x_modality;
 for i=1:3
     pair_keys = [pair_keys; standard+6*i; deviants_mapping_x_modality+6*i];
-    trial_keys = [trial_keys; trials_x_modality+12*i];
+    trial_keys = [trial_keys; trials_x_modality+6*i];
 end
 
-trial_keys = [trial_keys;trial_keys];
-trial_conditions = [trial_conditions;trial_conditions];
+%trial_keys = [trial_keys;trial_keys];
+%trial_conditions = [trial_conditions;trial_conditions];
 
 trials_order = randperm(size(trial_keys,1));
 
